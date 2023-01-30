b84565072820337664f141c372d8114cb95219c3
BellaBall <114452811+BellaBall@users.noreply.github.com>
2023-01-30 17:55:21 +0900

    Update ex1-3.js

--- ex1-3.js
+++ ex1-3.js
@@ -1,3 +1,3 @@
 function imgchange1() {document.getElementById('logo').src= './midnight1.png';}
 
-function imgchange2() {document.getElementById('logo').src= './midnight.png';}
+function imgchange2() {document.getElementById('logo').src= './midnight4.jpg';}
