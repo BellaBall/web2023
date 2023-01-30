b3d9953b8e47aac1e7b2f9f803e79a9b483978b1
BellaBall <114452811+BellaBall@users.noreply.github.com>
2023-01-30 18:18:06 +0900

    Update ex1-5.js

--- ex1-5.js
+++ ex1-5.js
@@ -1,5 +1,5 @@
 var who65 = 'world';function sayhello65() {alert('Hello, ' + "text" + '!');}
 
-function huseikai() {alert('残念...');}
+function huseikai() {alert('違う...');}
 
 function seikai() {alert('正解!!!');}
